847530431fd182f492257b8a6096e8730cac2117
Southbridge <7013162+southbridge-fur@users.noreply.github.com>
2025-06-11 16:10:12 -0400

    Alright time to finally add the store

---
+++ Content.Server/_Moffstation/Vampire/Components/BloodConsumptionComponent.cs
@@ -0,0 +1,30 @@
+﻿using Content.Server.Body.Components;
+
+namespace Content.Server._Moffstation.Vampire.Components;
+
+public sealed partial class BloodConsumptionComponent : Component
+{
+    /// <summary>
+    /// The next time an update will be performed
+    /// </summary>
+    [DataField]
+    public TimeSpan NextUpdate = TimeSpan.Zero;
+
+    /// <summary>
+    /// The interval between updates.
+    /// </summary>
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(5);
+
+    /// <summary>
+    /// Previous Blood Percentage
+    /// </summary>
+    [DataField]
+    public float PrevBloodPercentage = 0.5f;
+
+    /// <summary>
+    /// The maximum percentage of change per update.
+    /// </summary>
+    [DataField]
+    public float MaxChange = 0.1f;
+}
---
+++ Content.Server/_Moffstation/Vampire/EntitySystems/BloodConsumptionSystem.cs
@@ -0,0 +1,80 @@
+﻿using Content.Server._Moffstation.Vampire.Components;
+using Content.Server.Body.Components;
+using Content.Server.Body.Systems;
+using Content.Shared.Nutrition.Components;
+using Content.Shared.Nutrition.EntitySystems;
+using Robust.Shared.Timing;
+
+namespace Content.Server._Moffstation.Vampire.EntitySystems;
+
+public sealed class BloodConsumptionSystem : EntitySystem
+{
+    [Dependency] private readonly GameTiming _timing = default!;
+    [Dependency] private readonly BloodstreamSystem _bloodstreamSystem = default!;
+    [Dependency] private readonly HungerSystem _hungerSystem = default!;
+    [Dependency] private readonly ThirstSystem _thirstSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BloodConsumptionComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(EntityUid uid, BloodConsumptionComponent? component, MapInitEvent args)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        component.NextUpdate = _timing.CurTime + component.UpdateInterval;
+
+        if (!TryComp<BloodstreamComponent>(uid, out var bloodstream))
+            return;
+
+        component.PrevBloodPercentage = _bloodstreamSystem.GetBloodLevelPercentage(uid, bloodstream);
+
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var time = _timing.CurTime;
+        var enumerator = EntityQueryEnumerator<BloodConsumptionComponent>();
+        while (enumerator.MoveNext(out var uid, out var comp))
+        {
+            Update(uid, comp, time);
+        }
+    }
+
+    /// <summary>
+    /// Sets the percentage values of hunger and thirst to a percentage of the bloodstream.
+    /// </summary>
+    /// <param name="uid"></param>
+    /// <param name="comp"></param>
+    /// <param name="time"></param>
+    /// <remarks>
+    /// </remarks>
+    private void Update(EntityUid uid, BloodConsumptionComponent comp, TimeSpan time)
+    {
+        if (time < comp.NextUpdate)
+            return;
+
+        comp.NextUpdate += comp.UpdateInterval;
+
+        if (!TryComp<BloodstreamComponent>(uid, out var bloodstream))
+            return; // we need at least the blood stream before we can do something.
+
+        var bloodstreamPercentage = _bloodstreamSystem.GetBloodLevelPercentage(uid, bloodstream);
+        var modificationPercentage = Math.Clamp(
+            bloodstreamPercentage - comp.PrevBloodPercentage,
+            -comp.MaxChange,
+            comp.MaxChange);
+        if (TryComp<HungerComponent>(uid, out var hunger))
+            _hungerSystem.ModifyHunger(uid, modificationPercentage * hunger.Thresholds[HungerThreshold.Overfed], hunger);
+        if (TryComp<ThirstComponent>(uid, out var thirst))
+            _thirstSystem.ModifyThirst(uid, thirst, modificationPercentage * thirst.ThirstThresholds[ThirstThreshold.OverHydrated]);
+        comp.PrevBloodPercentage += modificationPercentage;
+    }
+
+}
