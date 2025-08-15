2f1b048bfa244398c44e2d9d1146823b37d3e081
Southbridge <7013162+southbridge-fur@users.noreply.github.com>
2025-08-15 14:48:10 -0400

    checkpointing

---
+++ Content.Server/_Moffstation/Spawning/Components/AvoidantSpawnPointComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Content.Server._Moffstation.Spawning.Components;
+
+/// <summary>
+/// Attached to a spawn point which avoids nearby players when spawning.
+/// </summary>
+[RegisterComponent]
+public sealed partial class AvoidantSpawnPointComponent : Component
+{
+    /// <summary>
+    /// The distance
+    /// </summary>
+    [DataField]
+    public float Range = 10f;
+}
---
+++ Content.Server/_Moffstation/Spawning/EntitySystems/AvoidantSpawnPointSystem.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using Content.Server._Moffstation.Spawning.Components;
+using Content.Server.GameTicking;
+using Content.Server.Spawners.Components;
+using Content.Server.Spawners.EntitySystems;
+using Content.Server.Station.Systems;
+using Content.Shared.Humanoid;
+using Robust.Shared.Map;
+using Robust.Shared.Random;
+
+namespace Content.Server._Moffstation.Spawning.EntitySystems;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class AvoidantSpawnPointSystem : EntitySystem
+{
+
+    [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly StationSystem _stationSystem = default!;
+    [Dependency] private readonly StationSpawningSystem _stationSpawning = default!;
+    [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<PlayerSpawningEvent>(OnPlayerSpawning, before: [typeof(SpawnPointSystem)]);
+    }
+
+    private void OnPlayerSpawning(PlayerSpawningEvent args)
+    {
+        if (args.SpawnResult != null)
+            return;
+
+        var points = EntityQueryEnumerator<AvoidantSpawnPointComponent, SpawnPointComponent, TransformComponent>();
+        var possiblePositions = new List<EntityCoordinates>();
+
+        while (points.MoveNext(out var uid, out var avoidantPoint, out var spawnPoint, out var xform))
+        {
+            if (args.Station != null && _stationSystem.GetOwningStation(uid, xform) != args.Station)
+                continue;
+
+            if ((_gameTicker.RunLevel == GameRunLevel.InRound && spawnPoint.SpawnType == SpawnPointType.LateJoin)
+                || (_gameTicker.RunLevel != GameRunLevel.InRound &&
+                spawnPoint.SpawnType == SpawnPointType.Job &&
+                (args.Job == null || spawnPoint.Job == args.Job)))
+            {
+
+                possiblePositions.Add(xform.Coordinates);
+            }
+        }
+
+        var spawnLoc = _random.Pick(possiblePositions);
+
+        args.SpawnResult = _stationSpawning.SpawnPlayerMob(
+            spawnLoc,
+            args.Job,
+            args.HumanoidCharacterProfile,
+            args.Station);
+    }
+
+    private bool PlayerInRange(Entity<AvoidantSpawnPointComponent, TransformComponent> entity)
+    {
+        var entities = new HashSet<Entity<HumanoidAppearanceComponent>>();
+        _entityLookup.GetEntitiesInRange(entity.Comp2.Coordinates, entity.Comp1.Range, entities);
+        return entities.Count > 0;
+    }
+}
